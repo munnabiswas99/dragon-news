20e11e0380648ce5acf2bce007d9add672ac4538
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-11-19 21:46:14 +0600

    Left Aside organized

---
+++ src/components/pages/CategoryNews.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useParams } from 'react-router';
+
+const CategoryNews = () => {
+    const {id} = useParams();
+    return (
+        <div>
+            <h2>Category News-{id}</h2>
+        </div>
+    );
+};
+
+export default CategoryNews;
