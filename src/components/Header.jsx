6a1aec89be7fe9ac5607acd8b27d176702dbf22a
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-11-19 18:49:51 +0600

    Marquee implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from "../assets/logo.png"       
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div>
             <div className="flex flex-col justify-center gap-3 items-center">
                 <img src={logo} alt="" />
-                <p>Journalism Without Fear or Favour</p>
+                <p className='text-accent'>Journalism Without Fear or Favour</p>
+                <p className='font-semibold text-accent'>{format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
             </div>
         </div>
     );
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../components/Header';
+import LatestNews from '../components/LatestNews';
 
 const HomeLayout = () => {
     return (
         <div>
             <header>
                 <Header></Header>
+                <section>
+                    <LatestNews></LatestNews>
+                </section>
             </header>
 
             <main>
